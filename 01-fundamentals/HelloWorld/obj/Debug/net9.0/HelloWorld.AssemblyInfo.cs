3c110cd90913b31c114ce892a50916961206fca7
cypress <comrie1.cc@gmail.com>
2026-01-04 09:40:41 +1300

    learning basic console commands + string interp which is diffrent to js

--- 01-fundamentals/HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ 01-fundamentals/HelloWorld/obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28381f3364a799b84a148c94619febfc3a40a588")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bc903ad79278cc480ff7cf1d6cb8d59eda45e40")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- notes/notes.md
+++ notes/notes.md
@@ -9,3 +9,20 @@ you can redeclare variables theyre all constant
 
 maths works the same as JS
 
+# console commands 
+
+.ReadKey is for checking for any button press 
+
+these are for changing the consoles colour
+Console.BackgroundColor = ConsoleColor.Yellow;
+Console.ForegroundColor = ConsoleColor.Black; 
+.clear() to whipe off all existing text
+.beep() to make a beep sound lol 
+
+first one would throw an error. you have to use a \ to display things that have commands assosiated with them 
+Console.WriteLine(""");
+Console.WriteLine("\""); 
+
+String interp doesnt need `` its $"" very diffrent 
+Console.WriteLine($"My favorite number is {myFavoriteNumber}.");
+
